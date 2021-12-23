f8d076764e04d3aa5fe737c05054b67a42f069b0
Nandakumar Edamana <nandakumar@epixelsolutions.com>
2021-12-23 08:50:31 +0530

    admin site: FilteredSelectMultiple widget for Person.projects

--- src/keytrack/admin.py
+++ src/keytrack/admin.py
@@ -4,6 +4,23 @@ from .models import Designation
 from .models import Person
 from .models import Project
 
+from django import forms
+
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
 admin.site.register(Designation)
-admin.site.register(Person)
 admin.site.register(Project)
+
+class PersonAdminForm(forms.ModelForm):
+	class Meta:
+		model   = Person
+		widgets = {
+			# is_stacked=False => lists placed horizontally
+			'projects': FilteredSelectMultiple('Projects', is_stacked=False),
+		}
+		fields  = '__all__'
+
+class PersonAdmin(admin.ModelAdmin):
+	form = PersonAdminForm
+
+admin.site.register(Person, PersonAdmin)
