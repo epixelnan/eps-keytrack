a2003de165bf4996b160b030361d108833be0615
Nandakumar Edamana <nandakumar@epixelsolutions.com>
2022-04-22 17:29:59 +0530

    started custom views

--- src/keytrack/urls.py
+++ src/keytrack/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name="login.html")),
+    path('', TemplateView.as_view(template_name="home.html"), name='home')
 ]
