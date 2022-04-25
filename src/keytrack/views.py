4a867f23d6430716093798d3f578d730f36819de
Nandakumar Edamana <nandakumar@epixelsolutions.com>
2022-04-25 16:40:51 +0530

    RegisterView: fix form not being saved; add email notification

--- src/keytrack/views.py
+++ src/keytrack/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.views import View
@@ -24,4 +26,28 @@ class RegisterView(CreateView):
 	fields = '__all__'
 	
 	def form_valid(self, form):
-		return render(self.request, 'register_success.html')
+		form.save()
+	
+		sentCount = send_mail(
+			'Registration Request Submitted',
+			'Your request to open an account on Epixel Keytrack has been submitted. '
+				'The support team will get back soon. Make sure to let us know if this '
+				'was not submitted by you.\r\n\r\nThank you.',
+			settings.EPS_EMAIL_FROM,
+			[form.cleaned_data['email']],
+			fail_silently=True,
+		)
+
+		# TODO proper logging
+		if sentCount != 1:
+			'error: could not send email to: ' + form.cleaned_data['email']
+
+		# TODO let the user know if error occured
+
+		return render(self.request, 'user/register_success.html')
+
+class RegisterRequestsView(View):
+	def get(self, request, *args, **kwargs):
+		cntxt = { 'regreqs': SelfRegisterRequest.objects.all() }
+		
+		return render(request, 'admin/regreqs.html', cntxt)
